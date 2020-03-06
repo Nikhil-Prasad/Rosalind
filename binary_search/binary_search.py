3c5e3ae0b4a754bea24079e5f8d6dd66eabbfbf8
Nikhil Prasad <prnikhil@umich.edu>
2020-03-05 21:17:10 -0500

    add rosalind files 3

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,9 +1,28 @@
 # Version that needs to add fiile pasrsing script. The output from rosalind is a file that has two arrays
 # and the length of each.
  
-with open('rosalind_bins.txt', 'r') as reader:
-    line = reader.readlines()
-    while line != '': # The EOF char is an empty string 
-        print(line, end='')
-        line = reader.readline()
 
+def binary_search(target,A):
+    low = 0 
+    high = len(A) - 1
+    # set the half way point, this needs to dynamically update
+
+    while low <= high: 
+        half = (low + high)//2
+        if A[half] == target:
+            return half +1 
+        
+        elif A[half] > target:
+            high = half -1 
+        
+        elif A[half] < target:
+            low = half + 1 
+    return -1 
+
+# have to generate list one and two now. 
+list1 = [10,20,30,40,50]
+list2 = [40,10,35,15,40,20]
+
+for i in range(0, len(list2)):
+    #output_list.append(binary_search(list2[i], list1))
+    print(binary_search(list2[i], list1))
