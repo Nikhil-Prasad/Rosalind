61336070c04c8096f09f76f93acb16005d0edd3b
Nikhil Prasad <prnikhil@umich.edu>
2020-04-13 22:10:57 -0400

    GC content refresh

---
+++ biology/computing_GC_Content/GC.py
@@ -0,0 +1,16 @@
+f = open('rosalind_gc.txt', 'r')
+string_file = f.read() #read in as a string so you can use split. 
+
+genes = string_file.split(">")[1:]
+gc = []
+keys = []
+
+for gene in genes:
+    GC_count = gene.count("C") + gene.count("G")
+    total_count = gene.count("C") + gene.count("G") + gene.count("A") + gene.count("T")
+    gc.append(float(GC_count)*100/total_count)
+    keys.append(gene[:13])
+
+print(keys[gc.index(max(gc))])
+print(max(gc))
+#print(genes[gc.index(max(gc))][:13]) #prints the first 13 character of the gene string in roder 
